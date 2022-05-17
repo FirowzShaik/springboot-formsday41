c4ed23798929c6a4470236e868b8042a937172f1
Jobiak <Jobiak@DESKTOP-GIO0BA7>
2022-05-17 10:26:25 +0530

    done

---
+++ springboot-formsday41/src/test/java/com/example/mvcforms/SpringbootFormsday41ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mvcforms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootFormsday41ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
